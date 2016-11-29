94a9061fba3416373ad9b0b9380612122992b19d
slindquist3 <slindquist3@gmail.com>
2016-11-29 15:25:39 -0500

    Done.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'redux';
+import counterReducer from './reducers/counterReducer';
+
+const store = createStore(counterReducer);
+
+console.log( store.getState() );
+store.dispatch({type: 'INCREMENT_COUNT'})
+store.dispatch({type: 'INCREMENT_COUNT'})
+console.log( store.getState() );
+store.dispatch({type: 'DECREMENT_COUNT'})
+console.log( store.getState() );
+store.dispatch({type: 'INCREMENT_COUNT'})
+store.dispatch({type: 'INCREMENT_COUNT'})
+console.log( store.getState() );
+store.dispatch({type: 'RESET_COUNT'});
+console.log( store.getState() );
---
+++ test/index-test.js
@@ -0,0 +1,5 @@
+describe('return true', function(){
+  it('should return true', function(){
+    true
+  })
+})
